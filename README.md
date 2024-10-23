e2677ee367afd02f40d81f356951082f8895d1ef
Pluto <devvaannsh@gmail.com>
2024-10-23 23:57:03 +0530

    feat: line height extension

--- README.md
+++ README.md
@@ -1,55 +1,30 @@
-# Extension Template
-
-Quick start and create a new extension by using this template. Follow the below 4 steps:
-
-Refer this [link](https://docs.phcode.dev/api/writing-ext-themes) for detailed extension docs after following the below steps:
-
-## step 1
-
-Login with github: https://github.com/login
-
-## step 2
-
-Create a new repository using this template.
-![New extension from template](https://user-images.githubusercontent.com/5336369/223931565-2708e516-a422-4e7b-9d89-9ac48c919c3d.gif)
-
-## step 3
-
-* Clone your GitHub Repo created from `step 2`. See [this link](https://docs.github.com/en/repositories/creating-and-managing-repositories/cloning-a-repository) to lean how to clone a repository from GitHub.
-* Edit `package.json` file in template, make sure to update the following fields accordingly:
-
-| Field       | Description                                                            |
-|-------------|------------------------------------------------------------------------|
-| `title`     | Replace "Name of the extension" with the actual title of your extension. |
-| `name`      | Change `github-<owner>-<repo>` to your specific package name, formatted as `github-yourusername-reponame`. |
-| `description` | Update to a brief, relevant description of what your extension does.  |
-| `version`   | Start with "0.0.1" or update to reflect your current version following semantic versioning. |
-| `license`   | Confirm "MIT" is suitable or specify another license if necessary.     |
-| `author`    | Replace with your name and a link to your GitHub profile or another URL. |
-| `homepage`  | Set to the URL of your project’s homepage or GitHub repository.        |
-| `engines`   | Ensure compatibility with the required Brackets version, e.g., ">=3.0.0". |
-| `categories`| Update "demo" with relevant categories that fit your extension.         |
-| `keywords`  | Update or append additional keywords that describe your extension.    |
-| `files`     | Ensure this includes all necessary files and folders for your extension. |
-
-## step 4
-
-* Go to https://create.phcode.dev .This is a special development centric website of phcode.dev which shows non minified js/css files in the browser developer tools.
-* Open the cloned folder and select `Debug > Load Project As Extension`
-
-![image](https://user-images.githubusercontent.com/5336369/224746152-0416a862-891a-4fe1-b9dd-09add25a6cc0.png)
+The Custom Line Height extension allows you to adjust the line height (line spacing) in your code editor, enhancing readability and reducing eye strain during coding sessions.
 
 
-* You can now make code changes to your extension and select `Debug> Reload With Extensions` to test the new code changes.
-* When you are done developing the extension/theme, select `Debug> Unload Project As Extension` to unload the extension.
+How to Use
 
-![image](https://user-images.githubusercontent.com/5336369/224747590-556dff1d-5b29-41c3-88a0-3ce72ab643d0.png)
+* Navigate to the Menu: Go to View > Line Height.
 
-# Detailed Documentation
+* Enter Your Value: Input your desired line height (from 0.5 to 8).
 
-Please go to [https://docs.phcode.dev/api/writing-ext-themes](https://docs.phcode.dev/api/writing-ext-themes) for more documentation/community support links.
+* Enjoy the Change: Your code will adjust to the new line height instantly.
 
-# Publishing your extension to the repository
+### Screenshots
+**Where to Find Line Height Settings:**
+![Line Height location](./assets/line-height.png)
 
-Once you have built your extension/theme, you can publish the extension to phcode.dev extension repository in a single step directly from this repo.
-Please see publish section in this link for more details: [Writing extension and themes](https://docs.phcode.dev/api/writing-ext-themes)
+**Line Height Adjustment Dialog:**
+![Line Height Dialog](./assets/line-height-dialog.png)
+
+**Line Height in Action**
+![Line Height Example](./assets/line-height-sample.png)
+
+Features
+
+* User-Friendly Interface: Quick adjustments with an easy input dialog.
+
+
+* Immediate Feedback: Changes are reflected instantly.
+
+
+* Customizable Range: Choose any line height value from 1 to 8.
--- main.js
+++ main.js
@@ -1,43 +1,129 @@
 /*global define, brackets, $ */
-
-// See detailed docs in https://github.com/phcode-dev/phoenix/wiki/How-To-Write-Extensions-And-Themes
-// A good place to look for code examples for extensions: https://github.com/phcode-dev/phoenix/tree/main/src/extensions/default
-
-// A simple extension that adds an entry in "file menu> hello world"
+// Simple extension that lets you change line height from the View menu
 define(function (require, exports, module) {
     "use strict";
-
-    // Brackets modules
+    
     const AppInit = brackets.getModule("utils/AppInit"),
         DefaultDialogs = brackets.getModule("widgets/DefaultDialogs"),
         Dialogs = brackets.getModule("widgets/Dialogs"),
         CommandManager = brackets.getModule("command/CommandManager"),
-        Menus = brackets.getModule("command/Menus");
+        Menus = brackets.getModule("command/Menus"),
+        EditorManager = brackets.getModule("editor/EditorManager"),
+        PreferencesManager = brackets.getModule("preferences/PreferencesManager"),
+        KeyBindingManager = brackets.getModule("command/KeyBindingManager");
 
-    // Function to run when the menu item is clicked
-    function handleHelloWorld() {
-        Dialogs.showModalDialog(
-            DefaultDialogs.DIALOG_ID_INFO,
-            "hello",
-            "world"
-        );
-    }
+    // Store user's last used value, defaulting to 1.5 since that's usually good for readability
+    const prefs = PreferencesManager.getExtensionPrefs("lineHeight");
+    prefs.definePreference("lastValue", "number", 1.5);
     
-      // First, register a command - a UI-less object associating an id to a handler
-    var MY_COMMAND_ID = "helloworld.sayhello";   // package-style naming to avoid collisions
-    CommandManager.register("Hello World", MY_COMMAND_ID, handleHelloWorld);
+    // Function to apply line height to all editors
+    function applyLineHeightToAllEditors(lineHeight) {
+        // Update all existing CodeMirror instances
+        $(".CodeMirror").css({
+            "line-height": lineHeight
+        });
+        
+        // Force refresh all editors
+        EditorManager.getActiveEditor()?.refresh();
+    }
 
-    // Then create a menu item bound to the command
-    // The label of the menu item is the name we gave the command (see above)
-    var menu = Menus.getMenu(Menus.AppMenuBar.FILE_MENU);
+    // Main function that handles the line height dialog and changes
+    function handleLineHeight() {
+        const lastValue = prefs.get("lastValue");
+        
+        const dialog = Dialogs.showModalDialog(
+            DefaultDialogs.DIALOG_ID_INFO,
+            "Set Line Height",
+            `<div style="margin-bottom: 15px;">
+                <label for="lineHeightInput">Enter Line Height (0.5 to 8):</label>
+                <input 
+                    type="number" 
+                    id="lineHeightInput" 
+                    min="0.5" 
+                    max="8" 
+                    step="0.1" 
+                    value="${lastValue}" 
+                    style="width: 100%; margin-top: 10px;" 
+                    required 
+                />
+            </div>
+            <div style="font-size: 12px; color: #666;">
+                Quick Tips:<br>
+                • Press Shift + L + H to open this dialog<br>
+                • Default line height is 1.5<br>
+                • Values between 1.2 and 1.8 are recommended for readability
+            </div>`,
+            [
+                {
+                    className: Dialogs.DIALOG_BTN_CLASS_PRIMARY,
+                    id: Dialogs.DIALOG_BTN_OK,
+                    text: "OK"
+                },
+                {
+                    className: Dialogs.DIALOG_BTN_CLASS_NORMAL,
+                    id: Dialogs.DIALOG_BTN_CANCEL,
+                    text: "Cancel"
+                }
+            ]
+        );
+
+        dialog.getElement().on('shown', function () {
+            dialog.getElement().find("#lineHeightInput").focus().select();
+        });
+
+        dialog.done(function (buttonId) {
+            if (buttonId === Dialogs.DIALOG_BTN_OK) {
+                const inputElement = dialog.getElement().find("#lineHeightInput");
+                const inputValue = inputElement.val();
+                
+                const lineHeightValue = parseFloat(inputValue);
+                
+                if (!isNaN(lineHeightValue) && lineHeightValue >= 0.5 && lineHeightValue <= 8) {
+                    // Save the value in preferences
+                    prefs.set("lastValue", lineHeightValue);
+                    
+                    // Apply to all editors
+                    applyLineHeightToAllEditors(lineHeightValue);
+                    
+                    console.log(`Line height set to ${lineHeightValue}`);
+                } else {
+                    Dialogs.showModalDialog(
+                        DefaultDialogs.DIALOG_ID_ERROR,
+                        "Error",
+                        "Please enter a valid number between 0.5 and 8."
+                    );
+                }
+            }
+        });
+    }
+
+    // Hook everything up to Phoenix Code
+    const MY_COMMAND_ID = "lineheight.set";
+    CommandManager.register("Line Height", MY_COMMAND_ID, handleLineHeight);
+
+    // Put it in the View menu since it's display-related
+    const menu = Menus.getMenu(Menus.AppMenuBar.VIEW_MENU);
     menu.addMenuItem(MY_COMMAND_ID);
     
-    // We could also add a key binding at the same time:
-    //menu.addMenuItem(MY_COMMAND_ID, "Ctrl-Alt-W");
-    // (Note: "Ctrl" is automatically mapped to "Cmd" on Mac)
-    
-    // Initialize extension once shell is finished initializing.
+    // Quick keyboard shortcut to save some clicks
+    KeyBindingManager.addBinding(MY_COMMAND_ID, {key: "Shift-L-H"});
+
+    // Get everything ready when Phoenix Code starts
     AppInit.appReady(function () {
-        console.log("hello world");
+        console.log("Line Height Extension loaded.");
+        
+        // Restore their last used line height setting
+        const lastValue = prefs.get("lastValue");
+        if (lastValue) {
+            applyLineHeightToAllEditors(lastValue);
+        }
+        
+        // Listen for new editor creation to apply line height
+        EditorManager.on("activeEditorChange", function () {
+            const lastValue = prefs.get("lastValue");
+            if (lastValue) {
+                applyLineHeightToAllEditors(lastValue);
+            }
+        });
     });
 });
